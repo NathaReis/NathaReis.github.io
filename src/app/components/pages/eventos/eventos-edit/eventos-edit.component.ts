930e1254dec2070d88c7af321e80ea855979f8de
NathaReis <nathan.david@siriusgroup.com.br>
2023-12-31 13:08:53 -0300

    Eventos ok
    Edito e excluo, valido o user e a data para saber se pode editar

--- src/app/components/pages/eventos/eventos-edit/eventos-edit.component.ts
+++ src/app/components/pages/eventos/eventos-edit/eventos-edit.component.ts
@@ -113,6 +113,11 @@ export class EventosEditComponent implements OnInit{
       this.isEventEdit = false;
       this.snack.openSnackBar('Edite com um dia de antecedência!', 2500)
     }
+    else if(event.user != String(localStorage.getItem('usermask_id')))
+    {
+      this.isEventEdit = false;
+      this.snack.openSnackBar('Edite apenas seus eventos!', 2500)
+    }
     let dataInicio: number | string = this.dateForString(new Date(this.dateBrForEUA(String(event.start_date))));
     dataInicio = +`${dataInicio.split("/")[2]}${dataInicio.split("/")[1]}${dataInicio.split("/")[0]}${event.start_time.replace(/\D/g, "")}`;
 
@@ -194,172 +199,7 @@ export class EventosEditComponent implements OnInit{
     return res;
   }
 
-  updateEvent()
-  {
-
-  }
-
-  deleteEvent()
-  {
-    
-  }
-  
- /*
-  constructor(
-    public dialogRef: MatDialogRef<DialogConfirmationComponent>,
-    private dataS: DataService,
-    private snack: SnackbarService,
-    private route: ActivatedRoute,
-    private router: Router,
-    private dialog: MatDialog,
-  ) {}
-
-  id: string = '';
-  isEventEdit = true;
-  ngOnInit(): void {
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = +date.getDate() + 1;
-    this.maxDate = new Date(year, 11, 31)
-    this.minDate = new Date(year, month, day)
-    this.agora = new Date(year, month, day);
-
-    //Busca eventos para validação de datas
-    this.getAllEvents();
-
-    this.id = String(this.route.snapshot.paramMap.get('id'));
-
-    //Para preencher os eventos
-    this.dataS.getEvent(String(this.id)).subscribe(event =>
-      {
-        this.preencherEvent(event.data())
-      })
-  }
-
-  event_name: string = '';
-  event_desc: string = '';
-  isOneDay: string = 'true';
-  start_date: Date = new Date();//'MM/DD/YYY'
-  end_date: Date = new Date();
-  start_time: string = '';
-  end_time: string = '';
-  maxDate: Date = new Date();
-  minDate: Date = new Date();
-  event_type: string = 'public';
-  agora: Date = new Date();
-  dataAntesdeEditar: number = 0;
-
-  eventsList: Event[] = [];
-  listDatas: Array<{data: {start: number, end: number}, hora: {start: number, end: number}, name: string, horario: string}> = [];
-  getAllEvents()
-  {
-    //Consulta o serviço Events
-    this.dataS.getAllEvents().subscribe(res =>
-      {
-        //Mapeia o resultado
-        this.eventsList = res.map((e: any) =>
-          {
-            const data = e.payload.doc.data();
-            data.id = e.payload.doc.id;
-            return data;
-          })
-        this.eventsList = this.eventsList
-        .filter(ev => ev.event_type == 'public' && ev.id != this.id);
-        this.listDatas = this.eventsList
-        .map(ev =>
-          {
-            const datInt = +`${ev.start_date.split("/")[2]}${ev.start_date.split("/")[1]}${ev.start_date.split("/")[0]}`;
-            const horInt = +`${ev.start_time.replace(/\D/g, "")}`;
-            const datFim = !eval(ev.isOneDay) 
-              ? +`${ev.end_date.split("/")[2]}${ev.end_date.split("/")[1]}${ev.end_date.split("/")[0]}`
-              :  datInt;
-            const horFim = +`${ev.end_time.replace(/\D/g, "")}`;
-            const name = ev.event_name;
-            const horario = `${ev.start_time}-${ev.end_time}`;
-            return {
-              data: {
-                start: datInt, 
-                end: datFim
-              }, 
-              hora: {
-                start: horInt, 
-                end: horFim
-              }, 
-              name: name,
-              horario: horario,
-            }
-          })
-      }, err => 
-      {
-        //Mensagem de erro
-        this.snack.openSnackBar(`Erro de busca: ${err}`);
-      })
-  }
-
-  dateBrForEUA(date: string)
-  {
-    let res = `${date.split('/')[1]}/${date.split('/')[0]}/${date.split('/')[2]}`;
-    return res;
-  }
-  dateForString(data: Date)
-  {
-    let date = String(data);
-    const year = date.slice(11,15);
-    const day = date.slice(8,10);
-    let month = date.slice(4,7)
-    switch(month)
-    {
-      case 'Jan':
-        month = '01'
-        break
-      case 'Feb':
-        month = '02'
-        break
-      case 'Mar':
-        month = '03'
-        break
-      case 'Apr':
-        month = '04'
-        break
-      case 'May':
-        month = '05'
-        break
-      case 'Jun':
-        month = '06'
-        break
-      case 'Jul':
-        month = '07'
-        break
-      case 'Aug':
-        month = '08'
-        break
-      case 'Sep':
-        month = '09'
-        break
-      case 'Oct':
-        month = '10'
-        break
-      case 'Nov':
-        month = '11'
-        break
-      case 'Dec':
-        month = '12'
-        break
-    }
-
-    date = `${day}/${month}/${year}`
-    return date;
-  }
-
-  horaNumberForHour(hora: number)
-  {
-    let str = String(hora);
-    let res = `${str.slice(0,1)}:${str.slice(2,3)}`;
-    return res;
-  }
-
-  validarEvento(): boolean
+  validarEvent(): boolean
   {
     if(this.event_name == '' || this.event_desc == '' || String(this.start_date) == '' || String(this.end_date) == '' || this.start_time == '' || this.end_time == '')
     {
@@ -434,9 +274,10 @@ export class EventosEditComponent implements OnInit{
       return true;
     }
   }
+
   criarEvent()
   {
-    if(this.validarEvento()) 
+    if(this.validarEvent()) 
     {
       return {
         event_name: this.event_name,
@@ -455,27 +296,7 @@ export class EventosEditComponent implements OnInit{
       return false;
     }
   }
-  preencherEvent(event: any)
-  {
-    let dataInicio: number | string = this.dateForString(new Date(this.dateBrForEUA(String(event.start_date))));
-    dataInicio = +`${dataInicio.split("/")[2]}${dataInicio.split("/")[1]}${dataInicio.split("/")[0]}${event.start_time.replace(/\D/g, "")}`;
 
-    let dataFim: number | string = this.agora != this.end_date ? this.dateForString(new Date(this.dateBrForEUA(String(event.end_date)))) : this.dateForString(new Date(this.dateBrForEUA(String(event.start_date))));
-    dataFim = +`${dataFim.split("/")[2]}${dataFim.split("/")[1]}${dataFim.split("/")[0]}${event.end_time.replace(/\D/g, "")}`;
-
-    const somaDatas = dataInicio + dataFim;
-    
-    this.event_desc = event.event_desc;
-    this.event_name = event.event_name;
-    this.isOneDay = eval(event.isOneDay) ? 'true' : '';
-    this.start_date = new Date(this.dateBrForEUA(event.start_date));//'MM/DD/YYY'
-    this.end_date = new Date(this.dateBrForEUA(event.end_date));
-    this.start_time = event.start_time;
-    this.end_time = event.end_time;
-    this.event_type = event.event_type;
-    this.dataAntesdeEditar = somaDatas;
-
-  }
   resetEvent()
   {
     this.event_desc = '';
@@ -487,16 +308,19 @@ export class EventosEditComponent implements OnInit{
     this.end_time = '';
     this.event_type = 'public';
   }
+
   updateEvent()
   {
     const event = this.criarEvent();
     if(event)
     {
-      this.dataS.updateEvent(event, this.id);
+      this.data.updateEvent(event, this.id);
       this.snack.openSnackBar('Atualizado com sucesso!');
       this.resetEvent();
+      this.router.navigate(['eventos']);
     }
   }
+
   deleteEvent()
   {
     const dialogRef = this.dialog.open(DialogConfirmationComponent, {
@@ -510,11 +334,11 @@ export class EventosEditComponent implements OnInit{
     dialogRef.afterClosed().subscribe((result: boolean) => {
       if(result)
       {
-        this.dataS.deleteEvent(this.id);
+        this.data.deleteEvent(this.id);
         this.snack.openSnackBar('Deletado com sucesso!');
         this.resetEvent();
+        this.router.navigate(['eventos']);
       }
     });
   }
-  */
 }
